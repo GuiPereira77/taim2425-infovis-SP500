d1d2494fdf7787a1578fca948c97a3248ada9bb6
IvaNikolova <73292635+IvaNikolova@users.noreply.github.com>
2024-11-13 22:17:25 +0000

    treemap color

--- treemap.py
+++ treemap.py
@@ -13,7 +13,7 @@ app.layout = html.Div([
         id='filter-options',
         value='shareholder',
         options=[
-            {'label': 'Shareholder', 'value': 'shareholder'},
+            {'label': 'Shareholders by Company', 'value': 'shareholder'},
             {'label': 'Sector', 'value': 'sector'},
             {'label': 'Industry', 'value': 'industry'},
             {'label': 'Country', 'value': 'country'}
@@ -28,8 +28,8 @@ app.layout = html.Div([
     Input("filter-options", "value"))
 
 def generate_chart(mode):
-    if  mode == 'shareholder':
-        merged_df["filter"] = merged_df["name"]
+    if mode == 'shareholder':
+        merged_df["filter"] = merged_df["name"]  
     elif mode == 'sector':
         merged_df["filter"] = merged_df["sector"]
     elif mode == 'industry':
@@ -43,12 +43,16 @@ def generate_chart(mode):
         values='value',
         color='value',
         hover_data=['shares'],
-        color_continuous_scale='RdBu'
+        color_continuous_scale='RdYlGn',    
     )
+    
 
-    fig.update_traces(root_color="lightgrey")
+    fig.update_traces(root_color="white")
     fig.update_traces(marker_line_width = 0)
-    fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
+    fig.update_layout(
+        margin = dict(t=50, l=25, r=25, b=25),
+        paper_bgcolor="lightgray",    # Set the entire figure background color
+        plot_bgcolor="lightgray")
 
     return fig
 
