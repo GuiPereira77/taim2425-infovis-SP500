70a9d1b7e68b09ad93325a8e18886af28cdc9fc4
IvaNikolova <73292635+IvaNikolova@users.noreply.github.com>
2024-11-14 11:48:45 +0000

    shareholder->company & shareholder->sector

--- treemap.py
+++ treemap.py
@@ -11,9 +11,10 @@ app.layout = html.Div([
     html.P("Values grouped by:"),
         dcc.Dropdown(
         id='filter-options',
-        value='shareholder',
+        value='shareholder_company',
         options=[
-            {'label': 'Shareholders by Company', 'value': 'shareholder'},
+            {'label': 'Shareholders by Company', 'value': 'shareholder_company'},
+            {'label': 'Shareholders by Sector', 'value': 'shareholder_sector'},
             {'label': 'Sector', 'value': 'sector'},
             {'label': 'Industry', 'value': 'industry'},
             {'label': 'Country', 'value': 'country'}
@@ -28,31 +29,65 @@ app.layout = html.Div([
     Input("filter-options", "value"))
 
 def generate_chart(mode):
-    if mode == 'shareholder':
-        merged_df["filter"] = merged_df["name"]  
+    if mode == 'shareholder_company':
+        # Path: sp500 > Shareholder Name > Company
+        fig = px.treemap(
+            merged_df,
+            path=[px.Constant("sp500"), 'name', 'symbol'],
+            values='value',
+            color='value',
+            hover_data=['shares'],
+            color_continuous_scale='RdYlGn',
+        )
+    elif mode == 'shareholder_sector':
+        # Path: sp500 > Shareholder Name > Sector
+        fig = px.treemap(
+            merged_df,
+            path=[px.Constant("sp500"), 'name', 'sector', 'symbol'],
+            values='value',
+            color='value',
+            hover_data=['shares'],
+            color_continuous_scale='RdYlGn',
+        )
     elif mode == 'sector':
-        merged_df["filter"] = merged_df["sector"]
+        # Path: sp500 > Sector > Symbol
+        fig = px.treemap(
+            merged_df,
+            path=[px.Constant("sp500"), 'sector', 'symbol'],
+            values='value',
+            color='value',
+            hover_data=['shares'],
+            color_continuous_scale='RdYlGn',
+        )
     elif mode == 'industry':
-        merged_df["filter"] = merged_df["industry"]
+        # Path: sp500 > Industry > Symbol
+        fig = px.treemap(
+            merged_df,
+            path=[px.Constant("sp500"), 'industry', 'symbol'],
+            values='value',
+            color='value',
+            hover_data=['shares'],
+            color_continuous_scale='RdYlGn',
+        )
     elif mode == 'country':
-        merged_df["filter"] = merged_df["country"]
+        # Path: sp500 > Country > Symbol
+        fig = px.treemap(
+            merged_df,
+            path=[px.Constant("sp500"), 'country', 'symbol'],
+            values='value',
+            color='value',
+            hover_data=['shares'],
+            color_continuous_scale='RdYlGn',
+        )
 
-    fig = px.treemap(
-        merged_df,
-        path=[px.Constant("sp500"), 'filter', 'symbol'],
-        values='value',
-        color='value',
-        hover_data=['shares'],
-        color_continuous_scale='RdYlGn',    
-    )
-    
 
-    fig.update_traces(root_color="white")
+    fig.update_traces(root_color="lightgray")
     fig.update_traces(marker_line_width = 0)
     fig.update_layout(
         margin = dict(t=50, l=25, r=25, b=25),
         paper_bgcolor="lightgray",    # Set the entire figure background color
-        plot_bgcolor="lightgray")
+        plot_bgcolor="lightgray"
+    )
 
     return fig
 
